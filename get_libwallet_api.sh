00a240a9e1844d12d8c446a9b33a20347758b36c
fuwa <fuwa25519@protonmail.com>
2020-04-13 02:59:00 +0800

    use wownero dev-v0.8 branch

--- get_libwallet_api.sh
+++ get_libwallet_api.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 MONERO_URL=https://github.com/wownero/wownero.git
-MONERO_BRANCH=master
+MONERO_BRANCH=dev-v0.8
 
 pushd $(pwd)
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -17,7 +17,7 @@ if [ ! -d $MONERO_DIR/src ]; then
 fi
 git submodule update --remote
 git -C $MONERO_DIR fetch
-git -C $MONERO_DIR checkout master
+git -C $MONERO_DIR checkout $MONERO_BRANCH
 
 # get monero core tag
 pushd $MONERO_DIR
